54acfb6cee15d7eddaf378a5ab6b7fe1ba8482d9
Eric Matte <ericmatte.inbox@gmail.com>
2020-04-20 17:58:29 -0400

    Add ha_url attribute for relative photo_attribute url

--- apps/media_lights_sync/media_lights_sync.py
+++ apps/media_lights_sync/media_lights_sync.py
@@ -7,7 +7,9 @@ from PIL import Image
 
 if sys.version_info < (3, 0):
     from urllib2 import urlopen
+    from urlparse import urljoin
 else:
+    from urllib.parse import urljoin
     from urllib.request import urlopen
 
 class MediaLightsSync(hass.Hass):
@@ -15,6 +17,7 @@ class MediaLightsSync(hass.Hass):
  
   def initialize(self):
     """Initialize the app and listen for media_player photo_attribute changes."""
+    self.ha_url = self.args.get("ha_url", None)
     self.condition = self.args.get("condition")
     self.lights = self.args["lights"]
     self.listen_state(self.change_lights_color, self.args["media_player"], attribute = self.args.get("photo_attribute", "entity_picture"))
@@ -22,7 +25,7 @@ class MediaLightsSync(hass.Hass):
   def change_lights_color(self, entity, attribute, oldUrl, newUrl, kwargs):
     """Callback when the photo_attribute has changed."""
     if newUrl != oldUrl and newUrl is not None and self.can_change_colors():
-      rgb_colors = self.get_colors(newUrl)
+      rgb_colors = self.get_colors(self.format_ha_url(newUrl))
       for i in range(len(self.lights)):
         threading.Thread(target=self.set_light_rgb, args=(self.lights[i], rgb_colors[i])).start()
 
@@ -45,3 +48,13 @@ class MediaLightsSync(hass.Hass):
   def extract_colors(self, palette, colors):
     """Extract an amount of colors corresponding to the amount of lights in the configuration."""
     return [palette[i:i + 3] for i in range(0, colors * 3, 3)]
+
+  def format_ha_url(self, url):
+    """Append ha_url if this is a relative url"""
+    is_relative = not url.startswith("http")
+    if not is_relative:
+      return url
+    elif is_relative and self.ha_url is None:
+      raise ValueError("ha_url must be specified when using relative url for photo_attribute.") 
+    else:
+      return urljoin(self.ha_url, url)
